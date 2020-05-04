c2f5ee0c191849c9f4b8431533d7a9a82861ac3b
virginie <virginiegodfrin@gmail.com>
2020-05-04 12:29:58 +0200

    chap4 Annotation & wildcard routes

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,11 +2,28 @@
 
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response; 
+use Symfony\Component\Routing\Annotation\Route;
 
 class QuestionController
 {
+    /**
+     * @Route("/")
+     */
     public function homepage() 
     {
-       return new Response("Hello controller");
+        return new Response("Hello my first controller");
+    }
+    
+    /**
+     * 
+     * @Route("/questions/{slug}")
+     */
+    public function show($slug) 
+    {
+//        dump($slug);
+        return new Response(sprintf(
+            'future page to the question "%s" !',
+            ucwords(str_replace('-',' ', $slug))
+        ));
     }
 }
