a56ab31d3f583b37c1d84836be4125f09240f974
virginie <virginiegodfrin@gmail.com>
2020-05-10 16:55:02 +0200

    chap08 The twig recipe

--- config/bundles.php
+++ config/bundles.php
@@ -1,6 +1,8 @@
 <?php
-
+// a bundle is a symfony plugin - corresponding to a feature
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
