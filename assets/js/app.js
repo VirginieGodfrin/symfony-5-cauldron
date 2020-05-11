56f73cb2d73d4cf82b35f1e485f959de2d93fa13
virginie <virginiegodfrin@gmail.com>
2020-05-11 11:44:39 +0200

    chap18 Webpack Encore: Javascript Greatness

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,21 @@
 import '../css/app.css';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
+import $ from 'jquery';
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
+$( document ).ready(function() {
+    var $container = $('.js-vote-arrows');
+    $container.find('a').on('click', function(e) {
+        e.preventDefault();
+        var $link = $(e.currentTarget);
+        console.log($link);
+        $.ajax({
+            url: '/comments/10/vote/'+$link.data('direction'),
+            method: 'POST'
+        }).then(function(data) {
+            $container.find('.js-vote-total').text(data.votes);
+        });
+    });
+});
